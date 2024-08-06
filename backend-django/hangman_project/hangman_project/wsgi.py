dc3baf605942c308eb52a0dc8a53ec723ef58327
Dan <Danevac@seznam.cz>
2024-08-06 23:13:04 +0200

    wsgi, vercel file changed

--- backend-django/hangman_project/hangman_project/wsgi.py
+++ backend-django/hangman_project/hangman_project/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hangman_project.settings")
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+
+app = application
